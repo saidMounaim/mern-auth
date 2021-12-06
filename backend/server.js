7008327939372c217a9500b4dd7be3d0ad010691
Said Mounaim <saidmounaim00@gmail.com>
2021-12-06 22:07:20 +0100

    Connect To Database With Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 const app = express();
 
 dotenv.config();
+
+connectDB();
 
 //
 app.get("/api/", (req, res) => {
