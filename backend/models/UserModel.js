78b4cb920570c266f628578f805c59c810449fb8
Said Mounaim <saidmounaim00@gmail.com>
2021-12-06 22:15:00 +0100

    Add User Model

---
+++ backend/models/UserModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
