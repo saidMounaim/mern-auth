f8f4c17338401e4689825cf9b645c95f85b9702e
Said Mounaim <saidmounaim00@gmail.com>
2021-12-07 22:50:11 +0100

    Add Error Handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middlewares/ErrorHandler.js";
 
 const app = express();
 
@@ -12,6 +13,9 @@ connectDB();
 app.get("/api/", (req, res) => {
   res.status(201).json({ success: true, message: "Hello " });
 });
+
+app.use(notFound());
+app.use(errorHandler());
 
 const PORT = process.env.PORT || 5000;
 
