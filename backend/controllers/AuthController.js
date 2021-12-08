12aebf8f066bb8105dd08b3d4df0791a1e4d6f44
Said Mounaim <saidmounaim00@gmail.com>
2021-12-08 21:41:22 +0100

    Install JWT & Generate Token When Registered

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,10 +1,19 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/UserModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // @DESC Register User
 // @ROUTE /api/users
 // @METHOD POST
 export const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create(req.body);
-  res.status(201).json({ success: true, data: user });
+  res.status(201).json({
+    success: true,
+    data: {
+      fullName: user.fullName,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user._id),
+    },
+  });
 });
