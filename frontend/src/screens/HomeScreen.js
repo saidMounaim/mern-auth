57dc8831c140d694adcf582aba583b130094fcba
Said Mounaim <saidmounaim00@gmail.com>
2021-12-11 13:49:37 +0100

    Add Protect Middleware

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
 import Register from "../components/Register";
 
 const HomeScreen = () => {
+  let navigate = useNavigate();
+  const userInfo = localStorage.getItem("userInfo");
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/profile");
+    }
+  }, []);
   return (
     <>
       <Register />
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Login from "../components/Login";
 
 const LoginScreen = () => {
+  let navigate = useNavigate();
+  const userInfo = localStorage.getItem("userInfo");
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/profile");
+    }
+  }, []);
+
   return (
     <>
       <Login />
